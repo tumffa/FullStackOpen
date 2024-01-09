931e476f34e5224a4e7131390c8f7306c7df5a01
tumffa <tuomas.v.niemi@gmail.com>
2024-01-09 20:50:43 +0200

    osa1/teht3

--- osa1/kurssitiedot/part1/src/App.jsx
+++ osa1/kurssitiedot/part1/src/App.jsx
@@ -1,20 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const exercises = [{part: part1, exercises: exercises1},
-     {part: part2, exercises: exercises2},
-      {part: part3, exercises: exercises3}]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  
 
   return (
     <div>
       <Header course={course} />
-      <Content props={exercises} />
-      <Total props={exercises} />
+      <Content props={[part1, part2, part3]} />
+      <Total props={[part1, part2, part3]} />
     </div>
   )
 }
@@ -51,7 +55,7 @@ const Total = ({ props }) => {
 const Part = ({ props }) => {
   return (
     <div>
-      <p>{props.part} {props.exercises}</p>
+      <p>{props.name} {props.exercises}</p>
     </div>
   )
 }
