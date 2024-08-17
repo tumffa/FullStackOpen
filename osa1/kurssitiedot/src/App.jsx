b1776f39b2d48bbbcab78081f8e1888b683f0f20
tumffa <tuomas.v.niemi@gmail.com>
2024-08-17 17:36:28 +0300

    Ex 1.4-1.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,25 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content props={parts} />
-      <Total props={parts} />
+      <Header course={course.name} />
+      <Content props={course.parts} />
+      <Total props={course.parts} />
     </div>
   )
 }
