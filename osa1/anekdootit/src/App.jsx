d9db9253083f9e321f52f32dc4a7d5ac1eb62a55
tumffa <tuomas.v.niemi@gmail.com>
2024-08-19 10:45:13 +0300

    ex1.14

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -27,10 +27,14 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <br/>has {votes[selected]} votes
       <br/><button onClick={() => giveVote({ index: selected })}>vote</button>
       <button onClick={changeAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[votes.indexOf(Math.max(...votes))]}
+      <br/>has {Math.max(...votes)}
     </div>
   )
 }
