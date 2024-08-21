9c65dc6a29d631e4fff82d65d4c310f85db86574
tumffa <tuomas.v.niemi@gmail.com>
2024-08-21 11:53:24 +0300

    ex2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: 
+          <input value={newName}
+          onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => 
+      <Person key={index} name={person.name}/>
+      )}
+    </div>
+  )
+
+}
+
+const Person = ({ name }) => <p>{name}</p>
+
+export default App
