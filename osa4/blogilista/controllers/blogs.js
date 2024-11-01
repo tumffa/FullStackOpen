c22270f5d92535378f578e325e8d9127b83fd0f6
tumffa <tuomas.v.niemi@gmail.com>
2024-11-01 14:09:48 +0200

    ex 4.8 v2

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-Blog
-    .find({})
-    .then(blogs => {
-    response.json(blogs)
-    })
-})
+blogsRouter.get('/', async (request, response) => {
+    try {
+      const blogs = await Blog.find({})
+      response.json(blogs)
+    } catch (error) {
+      response.status(500).json({ error: 'Something went wrong' })
+    }
+  })
 
 blogsRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
