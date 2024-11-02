72d7d06168cbe77d06c0d71d80c2f38495ebb25a
tumffa <tuomas.v.niemi@gmail.com>
2024-11-02 12:12:59 +0200

    ex 4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,6 +3,7 @@ const app = express()
 const config = require('./utils/config')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 
@@ -12,6 +13,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
