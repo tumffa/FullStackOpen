c37cc758d843ad10359b28cbe617bf1f814dabf7
tumffa <tuomas.v.niemi@gmail.com>
2024-11-22 18:03:35 +0200

    ex 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,27 +1,54 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import { expect } from 'vitest'
+
+const blog = {
+  title: 'Component testing is done with react-testing-library',
+  author: 'Tester',
+  url: 'http://localhost',
+  likes: 5,
+}
+
+const mockHandleLike = vi.fn()
+const mockHandleDelete = vi.fn()
+const testUser = { username: 'testuser' }
 
 test('renders content', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library'
-  }
-
-  const mockHandleLike = vi.fn()
-  const mockHandleDelete = vi.fn()
-  const user = { username: 'testuser' }
-
   render(
   <Blog 
     blog={blog} 
     handleLike={mockHandleLike} 
     handleDelete={mockHandleDelete}
-    user={user}
+    user={testUser}
     />
   )
 
   expect(
-    screen.getByText(
+    screen.findByText(
       'Component testing is done with react-testing-library'
     )
   ).toBeDefined()
+})
+
+test('renders full content when view button is clicked', () => {
+  render(
+    <Blog
+      blog={blog}
+      handleLike={mockHandleLike}
+      handleDelete={mockHandleDelete}
+      user={testUser}
+    />
+  )
+
+  const button = screen.getByText('view')
+  userEvent.click(button)
+
+  expect(screen.findByText(
+    'Component testing is done with react-testing-library'
+  )).toBeDefined()
+  expect(screen.findByText('Tester')).toBeDefined()
+  expect(screen.findByText('http://localhost')).toBeDefined()
+  expect(screen.findByText('likes 5')).toBeDefined()
+  expect(screen.findByText('testuser')).toBeDefined()
 })
