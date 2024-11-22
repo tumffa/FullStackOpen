3d63faa3b4d1758f1948da8aa22644af2ddccd71
tumffa <tuomas.v.niemi@gmail.com>
2024-11-22 12:05:00 +0200

    ex 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      <div> 
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      <div style={{ display: visible ? '' : 'none' }}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        {blog.user && blog.user.username && (
+          <div>{blog.user.username}</div>
+        )}
+      </div>
+  </div>
+)}
 
 export default Blog
