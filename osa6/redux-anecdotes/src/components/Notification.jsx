851cf7cde21ffd4dbf5267bc39e923c87c7232a6
tumffa <tuomas.v.niemi@gmail.com>
2024-12-23 18:27:16 +0200

    ex 6.12

---
+++ osa6/redux-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux'
+import { setNotification } from '../reducers/notificationReducer'
+
+const Notification = () => {
+  const notification = useSelector(state => state.notification)
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  return (
+    <div style={style}>
+      {notification}
+    </div>
+  )
+}
+
+export default Notification
