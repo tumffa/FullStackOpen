c9a8d0da6e6258c941fbf9ca4dc92550b9a3c166
tumffa <tuomas.v.niemi@gmail.com>
2025-01-03 16:36:38 +0200

    ex 6.22

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,12 +1,22 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, voteAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const votedAnecdoteMutation = useMutation({ 
+    mutationFn: (anecdote) => voteAnecdote(anecdote),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
 
   const handleVote = (anecdote) => {
     console.log('vote')
+    const votedAnecdote = { ...anecdote, votes: anecdote.votes + 1 }
+    votedAnecdoteMutation.mutate(votedAnecdote)
   }
 
   const result = useQuery({
--- osa6/query-anecdotes/src/requests.js
+++ osa6/query-anecdotes/src/requests.js
@@ -5,3 +5,6 @@ export const getAnecdotes = () =>
 
 export const createAnecdote = (anecdote) =>
   axios.post('http://localhost:3001/anecdotes', anecdote).then(res => res.data)
+
+export const voteAnecdote = (anecdote) =>
+  axios.put(`http://localhost:3001/anecdotes/${anecdote.id}`, anecdote).then(res => res.data)
