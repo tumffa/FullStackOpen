c547e02972ddff2a81f2cd0419da2752cb433492
tumffa <tuomas.v.niemi@gmail.com>
2025-01-03 16:26:22 +0200

    ex 6.21

--- osa6/query-anecdotes/src/requests.js
+++ osa6/query-anecdotes/src/requests.js
@@ -2,3 +2,6 @@ import axios from 'axios'
 
 export const getAnecdotes = () =>
   axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+
+export const createAnecdote = (anecdote) =>
+  axios.post('http://localhost:3001/anecdotes', anecdote).then(res => res.data)
