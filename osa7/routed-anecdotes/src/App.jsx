e73c40bcbf2267231ad1a0c3b042147a1b658a0c
tumffa <tuomas.v.niemi@gmail.com>
2025-01-07 15:39:19 +0200

    ex 7.4

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { 
   Routes, Route, Link, useMatch, useNavigate
 } from 'react-router-dom'
+import useField from './hooks/index'
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
@@ -53,17 +54,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-  const navigate = useNavigate()
 
+  const navigate = useNavigate()
+  const contentField = useField('content')
+  const authorField = useField('author')
+  const infoField = useField('info')
+  
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: contentField.value,
+      author: authorField.value,
+      info: infoField.value,
       votes: 0
     })
     navigate('/')
@@ -75,15 +77,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...contentField} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...authorField} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...infoField} />
         </div>
         <button>create</button>
       </form>
